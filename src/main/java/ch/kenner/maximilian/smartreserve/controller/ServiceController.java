a8e8f9f2e316c792d4cdb0a2f667b837dc8755f2
Max <maximilian.kenn@gmail.com>
2026-05-04 14:26:16 +0200

    Feature/security keycloak (#2)
    * adds security, also adds service putMapping
    
    * adds authorization button to swagger-ui

--- src/main/java/ch/kenner/maximilian/smartreserve/controller/ServiceController.java
+++ src/main/java/ch/kenner/maximilian/smartreserve/controller/ServiceController.java
@@ -2,13 +2,19 @@ package ch.kenner.maximilian.smartreserve.controller;
 
 import ch.kenner.maximilian.smartreserve.base.MessageResponse;
 import ch.kenner.maximilian.smartreserve.model.Service;
+import ch.kenner.maximilian.smartreserve.security.Roles;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@SecurityRequirement(name = "bearerAuth")
+@Validated
 public class ServiceController {
     private final ServiceService serviceService;
 
@@ -17,21 +23,31 @@ public class ServiceController {
     }
 
     @GetMapping("api/service")
+    @RolesAllowed(Roles.Guest)
     public ResponseEntity<List<Service>> getAllServices() {
         return ResponseEntity.ok(serviceService.getAllServices());
     }
 
     @GetMapping("api/service/{id}")
+    @RolesAllowed(Roles.Guest)
     public ResponseEntity<Service> getServiceById(@PathVariable Long id) {
         return ResponseEntity.ok(serviceService.getServiceById(id));
     }
 
     @PostMapping("api/service")
+    @RolesAllowed(Roles.Admin)
     public ResponseEntity<Service> postService(@Valid @RequestBody Service service) {
         return ResponseEntity.ok(serviceService.insertService(service));
     }
 
+    @PutMapping("api/service/{id}")
+    @RolesAllowed(Roles.Admin)
+    public ResponseEntity<Service> putService(@PathVariable Long id, @Valid @RequestBody Service service) {
+        return ResponseEntity.ok(serviceService.updateservice(service, id));
+    }
+
     @DeleteMapping("api/service/{id}")
+    @RolesAllowed(Roles.Admin)
     public ResponseEntity<MessageResponse> deleteService(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(serviceService.deleteService(id));
