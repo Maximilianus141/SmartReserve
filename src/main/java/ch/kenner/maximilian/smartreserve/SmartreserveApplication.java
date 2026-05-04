bd996c78519cb1b4fb7e3387f4febc93b78ad391
Cowycorn <maximilian.kenn@gmail.com>
2026-05-04 10:55:03 +0200

    initial

---
+++ src/main/java/ch/kenner/maximilian/smartreserve/SmartreserveApplication.java
@@ -0,0 +1,13 @@
+package ch.kenner.maximilian.smartreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartreserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartreserveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/kenner/maximilian/smartreserve/SmartreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kenner.maximilian.smartreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartreserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
