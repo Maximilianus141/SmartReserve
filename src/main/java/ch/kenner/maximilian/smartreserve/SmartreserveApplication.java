9a133bb43469c61590fc76da7810d06aecc9c20c
Max <maximilian.kenn@gmail.com>
2026-05-04 11:43:29 +0200

    removes security dependencies for now

--- src/main/java/ch/kenner/maximilian/smartreserve/SmartreserveApplication.java
+++ src/main/java/ch/kenner/maximilian/smartreserve/SmartreserveApplication.java
@@ -2,11 +2,10 @@ package ch.kenner.maximilian.smartreserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class SmartreserveApplication {
 
     public static void main(String[] args) {
