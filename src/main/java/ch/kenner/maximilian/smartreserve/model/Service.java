345dcece5f3470601de3257d4ec15fe2314fc3bd
Max <maximilian.kenn@gmail.com>
2026-05-04 11:10:18 +0200

    adds Service Entity and ServiceRepository

---
+++ src/main/java/ch/kenner/maximilian/smartreserve/model/Service.java
@@ -0,0 +1,35 @@
+package ch.kenner.maximilian.smartreserve.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DialectOverride;
+
+import java.time.Duration;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Service {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(max = 255)
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String description;
+
+    @NotEmpty
+    private Duration duration;
+
+
+    private Duration afterServiceBreakDuration = Duration.ZERO;
+}
